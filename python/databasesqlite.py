356b68733f4381ed132eb78b18be5ba2143c475d
akhilkumar3210 <ak2072719@gmail.com>
2024-10-29 15:58:39 +0530

    sqlite

--- python/databasesqlite.py
+++ python/databasesqlite.py
@@ -34,6 +34,35 @@ except:
 # name1=input("Enter a  old Name : ")
 # con.execute("update user set name=?,age=? where name=? ",(name,age,name1))
 # con.commit()
-no=int(input("Entet a Number : "))
-con.execute("delete from user where no=?",(no,))
-con.commit()
+# no=int(input("Entet a Number : "))
+# con.execute("delete from user where no=?",(no,))
+# con.commit()
+
+# data=con.execute("select * from user where name like '%u' ")
+# data=con.execute("select * from user where name like '______' ")
+
+# data=con.execute("select * from user order by name ")
+# data=con.execute("select * from user order by name  desc ")
+# for i in data:
+#     print(i)
+
+try:
+    con.execute("create table address(no int,place text,pin int)")
+except:
+    print("table created ")
+
+# con.execute("insert into user(no,name,age)values(1,'jerin',32)")
+# con.commit()
+#data=con.execute("select user.no,user.name,user.age,address.place,address.pin from user inner join address on user.no=address.no ")
+# data=con.execute("select user.no,user.name,user.age,address.place,address.pin from user left join address on user.no=address.no ")
+# data=con.execute("select user.no,user.name,user.age,address.place,address.pin from user left join address on user.no=address.no ")
+# for i in data:
+#     print(i)
+
+# data=con.execute("select name,min(age) from user group by name")
+# data=con.execute("select name,max(age) from user group by name")
+# data=con.execute("select name,sum(age) from user group by name")
+# data=con.execute("select name,avg(age) from user group by name")
+data=con.execute("select name,count(age) from user group by name")
+for i in data:
+    print(i)
